784fe1d637bdcae057e95e43a67cc725893aac28
Pramod Sasi <pramodsasi@Pramods-MacBook-Air.local>
2026-07-21 20:23:37 +0530

    Fix NSG subnet associations, password complexity, and RBAC timing

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ data "azurerm_client_config" "current" {}
 resource "random_password" "windows_admin_password" {
   length      = 16
   special     = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
   min_upper   = 1
   min_lower   = 1
   min_numeric = 1
